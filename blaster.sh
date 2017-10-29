d402e723eee91d070f6df3811a7dc2c3d596cc21
Inventor <maxfan8@gmail.com>
2017-10-29 12:57:48 -0400

    Added disclaimer and switched to dnsblast

--- blaster.sh
+++ blaster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-##This is a shell dnsblaster
+#This is a shell dnsblaster to stress-test pihole
 
 ## Deps
 WHATITIS=dnsutils
@@ -29,17 +29,16 @@ apt-get install -y $WHATPACKAGE
 fi
 
 ## Vars
-HOSTTOBLAST=$(whiptail --inputbox "What DNS SERVER?" 10 80 "" 3>&1 1>&2 2>&3)
-DOMAINLISTURL="https://github.com/deathbybandaid/piholeparser/raw/master/Subscribable-Lists/CombinedBlacklists/CombinedBlackLists.txt"
-DOMAINLIST="/home/domainlist"
+HOSTTOBLAST=$(whiptail --inputbox "What DNS server would you like to blast?" 10 80 "" 3>&1 1>&2 2>&3)
+AMOUNTOFBLASTS=$(whiptail --inputbox "How many requests would you like to send?" 10 80 "" 3>&1 1>&2 2>&3)
+whiptail --title "Disclamer" --msgbox "The developers of this program are not responsible for any excessive load caused by this program. By selecting OK, you have made yourself aware of the potential repercussions of your actions. To exit, press control-c." --cancel-button Cancel 10 80
 
-## Get Domain List
-wget -q -O $DOMAINLIST $DOMAINLISTURL
+sudo git clone https://github.com/jedisct1/dnsblast /etc/dnsblast
+cd /etc/dnsblast
+sudo make
+/etc/dnsblast/dnsblast $HOSTTOBLAST $AMOUNTOFBLASTS
 
-for source in `cat $DOMAINLIST`;
-do
-echo "Querying $source"
-dig @$HOSTTOBLAST $source > /dev/null
+rm -r
 
 ## End Loop
 done
